6bb3705759ec022a7700002ee15f2b56778bc5eb
Andrea-Boldrini <2001.boldrini@gmail.com>
2025-05-14 16:09:37 +0800

    Add files via upload

---
+++ tiltrotor_drone_LMPC/include/wrench_estimator.hpp
@@ -0,0 +1,42 @@
+#ifndef WRENCH_ESTIMATOR_HPP
+#define WRENCH_ESTIMATOR_HPP
+
+#include <Eigen/Dense>
+#include <chrono>
+#include <utility> 
+
+class WRENCHEstimator{
+public:
+    WRENCHEstimator();
+
+    std::pair<Eigen::VectorXd, Eigen::Vector3d> computeWrench(
+        const Eigen::Vector3d& attitude,
+        const Eigen::Vector3d& velocity,
+        Eigen::Vector3d& rates,
+        double T,
+        const Eigen::VectorXd& rotors_thrust,
+        double tilt,
+        double m,
+        double a,
+        double tau,
+        double I_x,
+        double I_y,
+        double I_z,
+        double g,
+        double kf,
+        double Lv,
+        double Lh
+    );
+
+private:
+    double dt;
+    std::chrono::high_resolution_clock::time_point _t0, _t1;
+    
+    Eigen::Matrix3d Q_prev;
+    Eigen::VectorXd gamma;   
+    Eigen::VectorXd W_f;  
+    Eigen::VectorXd W_f_body;       
+};
+
+
+#endif
